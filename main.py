f51a58dd6a018fd25cbacff9170d9b69b4a550e6
yvan.kombou <yvan.kombou@kaeyros-analytics.com>
2022-04-02 11:50:53 +0200

    =begin analyse page contact

---
+++ main.py
@@ -0,0 +1,4 @@
+import pages.contact as contact
+
+if __name__ == '__main__':
+    print(contact.start())
---
+++ pages/contact.py
@@ -0,0 +1,7 @@
+import analytics
+
+
+def start():
+    return analytics.page('user_id', 'Docs', 'Python', {
+        'url': 'http://festival.kaeyros-analytics.com/contact'
+    })
